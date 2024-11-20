35748a8eb15b826a22e6e7d923b68be099162de3
Ander <cifuentees224@gmail.com>
2024-11-19 21:16:02 -0500

    Update API endpoints to new server address

--- src/components/auth/Register.tsx
+++ src/components/auth/Register.tsx
@@ -26,7 +26,7 @@ const Register: React.FC = () => {
       formData.append('usuario', usuario);
       formData.append('clave', clave);
 
-      await axios.post('http://3.144.86.141:5000/clientes/', formData);
+      await axios.post('http://18.118.26.128:5000/clientes/', formData);
 
       alert('Usuario registrado exitosamente');
       navigate('/');
